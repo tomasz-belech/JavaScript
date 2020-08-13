856c92b9355cb6ef6a95623bc9b8ae928362f9b3
Tomasz Belech <tomaszbelech1@gmail.com>
2020-08-13 13:09:58 +0200

    Creating a new JavaScript Object

--- test.js
+++ test.js
@@ -1,7 +1,24 @@
-var string1 = "abc";
-var string2 = "bcd";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-console.log(string1 < string2);
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "TOmek";
+myCar.drive = function(){ console.log("now driving");};
+
+myCar.drive();
+
+var myCar2 = {
 
 
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+         console.log(speed * time);}
+
+};
+
+ console.log(myCar2.maxSpeed);
+ myCar2.drive(50, 3);
 
