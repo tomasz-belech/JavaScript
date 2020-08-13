eb127a68b17b39294e158acf6cddc601f6590533
Tomasz Belech <tomaszbelech1@gmail.com>
2020-08-13 02:40:34 +0200

    Else If Statements

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
-var youLikeMeat = false;
-var myNum = 8;
+var myAge = 49;
 
-if (myNum == 10){
+if (myAge > 30){
+    
+    document.write("you are over 30!");
 
-    document.write("myNum is equal to 10");
+} else if (myAge > 20){
 
+
+    document.write("you are over 20!");
+
+} else if (myAge > 10){
+
+    document.write("you are over 10!");
 
 } else{
-    document.write("myNum is not equal to 10");
+
+    document.write("you are not over 10!");
 
 }
-
 
