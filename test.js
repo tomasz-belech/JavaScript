9ccf3a1157fb01aa7068326e2826aa36a7ea96ae
Tomasz Belech <tomaszbelech1@gmail.com>
2020-08-13 04:11:46 +0200

    Strings

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
-var a = 6;
-var b = 5;
+var string1 = "abc";
+var string2 = "bcd";
 
-if (isNaN(a)){
+console.log(string1 < string2);
 
-    console.log("that aint even a number, thickie");
 
-} else{
-
-console.log("mearing of life is " + (a + b));
-
-}
 
