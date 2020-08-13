aa0dc001aefeedacf3d19f41d0504b723e760d19
Tomasz Belech <tomaszbelech1@gmail.com>
2020-08-13 03:27:35 +0200

    Functions

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-var links = document.getElementsByTagName("a");
+function getAverage (a,b,c,d,e,f){
 
-for(i = 0; i < links.length; i++){
-
-    links[i].className = "link-" + i;
+    var average = (a + b + c + d + e + f) / 6;
+    console.log(average);
+    return average;
 
 }
+
+ var myResult = getAverage(7,8,9,10,11,12);
+ console.log("the average is " + myResult);
